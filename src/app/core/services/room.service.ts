cfe741dc1e6832a7ba5d42eb4ec1de9309c115b5
Robert Boyd Seitz <seitzrb@lc.gov>
2019-03-14 12:38:19 -0600

    Changed URL in service to local variable

--- src/app/core/services/room.service.ts
+++ src/app/core/services/room.service.ts
@@ -11,14 +11,15 @@ import { BaseService } from '../base.service';
 
 export class RoomService extends BaseService {
   // api/products/products.json
-    roomUrlx = "https://vard12.lc.gov/dataservices/WebApi/RoomSetup/api/Room";
-    roomUrl = "src/api/roomdata/rooms.json"
+    roomUrl = "https://vard12.lc.gov/dataservices/WebApi/RoomSetup/api/Room";
+    roomUrlx = "src/api/roomdata/rooms.json"
 
     constructor(private http: HttpClient) { super(); }
 
     getRooms(): Observable<Room[]> {
+      let roomUrl = this.roomUrl
       // const options = { headers: new HttpHeaders({'Accept': 'application/json'})};
-      return this.http.get<Room[]>(this.roomUrl)
+      return this.http.get<Room[]>(roomUrl)
          .pipe (
              catchError((err) => 
                   throwError(err.Message)
